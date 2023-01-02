70dd2378df59d0cb48e5699b4927dc05e7a5d8c9
Emmanuel Yusuf <emaz4me@gmail.com>
2023-01-02 16:47:59 +0100

    update

--- features/international/InternationalHero.tsx
+++ features/international/InternationalHero.tsx
@@ -17,20 +17,28 @@ export const InternationalHero = () => {
       <MainContainer>
         <SimpleGrid columns={[1, 1, 1, 2]} spacing="3rem" py="2rem">
           <Center flexDir="column" alignItems="flex-start" pos="relative">
-            <Heading fontSize={["6xl", "6xl", "6xl", "8xl"]} color="brand.dark.200">
-              Educating{' '}
+            <Heading
+              fontSize={['6xl', '6xl', '6xl', '7xl']}
+              color="brand.dark.200"
+            >
+              International{' '}
               <Box color="brand.purple.500" as="span" textTransform="uppercase">
-                career switchers
+                PRODUCT Design
               </Box>{' '}
               &{' '}
               <Box color="brand.pink.700" as="span" textTransform="uppercase">
-                product teams.
+                training.
               </Box>
             </Heading>
 
-            <Text fontSize={["xl", "xl", "xl", "3xl"]} color="brand.dark.200" mt="0.75rem">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <Text
+              fontSize={['xl', 'xl', 'xl', '3xl']}
+              color="brand.dark.200"
+              mt="0.75rem"
+              maxW="524px"
+            >
+              Build design skills and soft skills to solve problems on a global
+              scale in international companies
             </Text>
 
             <Button h="3rem" mt="1.25rem">
@@ -55,17 +63,36 @@ export const InternationalHero = () => {
       <MainContainer bg="brand.purple.500">
         <Flex alignItems="center" justifyContent="space-between" py="1.875rem">
           <Box>
-            <Heading textTransform="uppercase" fontSize={["2xl", "2xl", "2xl", "4xl"]} color="brand.white">
-                We have the vision to train in 2023{' '}
+            <Heading
+              textTransform="uppercase"
+              fontSize={['2xl', '2xl', '2xl', '4xl']}
+              color="brand.white"
+            >
+              We have the vision to train in 2023{' '}
             </Heading>
-            <Heading textTransform="uppercase" fontSize={["7xl", "7xl", "7xl", "9xl"]} color="brand.white">2000+</Heading>
+            <Heading
+              textTransform="uppercase"
+              fontSize={['7xl', '7xl', '7xl', '9xl']}
+              color="brand.white"
+            >
+              2000+
+            </Heading>
           </Box>
 
-          <Button h="3.75rem" w="12.75rem" variant="outline" borderColor="brand.white" color="brand.white" _hover={{
-            bg: 'brand.yellow.500',
-            color: 'brand.dark.200',
-            borderColor: 'brand.yellow.500',
-          }}>Enroll Now</Button>
+          <Button
+            h="3.75rem"
+            w="12.75rem"
+            variant="outline"
+            borderColor="brand.white"
+            color="brand.white"
+            _hover={{
+              bg: 'brand.yellow.500',
+              color: 'brand.dark.200',
+              borderColor: 'brand.yellow.500',
+            }}
+          >
+            Enroll Now
+          </Button>
         </Flex>
       </MainContainer>
     </Box>
