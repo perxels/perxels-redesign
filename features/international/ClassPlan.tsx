e3e55774b9c46c3a32ba845a71103508d1a581a1
Emmanuel Yusuf <emaz4me@gmail.com>
2023-01-02 20:37:37 +0100

    update

--- features/international/ClassPlan.tsx
+++ features/international/ClassPlan.tsx
@@ -13,6 +13,7 @@ export const ClassPlan = () => {
 
       <ClassDetails
         title="International"
+        id="#"
         classDur="9 Weeks."
         classTime="2-3 times a week."
         installments={["70% On Admission;", "30% after one month."]}
