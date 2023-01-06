6664f5f1c741311a9b93ea206465007c88b66e06
Emmanuel Yusuf <emaz4me@gmail.com>
2023-01-06 19:54:28 +0100

    trying to fix zooming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,22 @@ import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { theme } from '../config/theme'
 
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import "../styles/globals.css"
-import { Provider } from 'react-redux';
-import { store } from '../state/store';
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import '../styles/globals.css'
+import { Provider } from 'react-redux'
+import { store } from '../state/store'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
       <ChakraProvider theme={theme}>
         <Component {...pageProps} />
       </ChakraProvider>
