c26ffdd57fd037f14473e16950d99fa610d62d18
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-01-30 09:32:56 +0100

    newBanner

--- constant/bannerContent.tsx
+++ constant/bannerContent.tsx
@@ -26,10 +26,10 @@ export const bannerContent: BannerContent = {
   bannerImage: '/assets/images/banner/bannerImage.jpeg',
   description:
     "Are you interested in designing products that are usable and useful for a wide range of users?",
-  content1: "Introducing stakeholders to accessibility can help ensure that your product meets legal requirements, has a better user experience, and has a competitive advantage. ",
+  content1: "Introducing stakeholders to accessibility can help ensure that your product meets legal requirements, a better user experience, and has a competitive advantage. ",
   content2: "In this session, we will explore the many benefits of accessibility and provide tips on how to effectively introduce stakeholders to the principles of inclusive design. ",
   content3: "Don't miss this opportunity to learn how accessibility can help make your products more successful and improve your company's reputation!",
   content4: "",
   speakerName: 'Victoria Ottah',
-  speakerRole: 'Product Designer',
+  speakerRole: 'Product Designer, ToriasDesign',
 }
