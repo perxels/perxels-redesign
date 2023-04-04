8ac4c01053651d617faa9c3ba3809b9f6b372062
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-04-04 10:57:20 +0100

    event

--- constant/bannerContent.tsx
+++ constant/bannerContent.tsx
@@ -17,17 +17,17 @@ interface BannerContent {
 
 export const bannerContent: BannerContent = {
   mainTitle: ' ',
-  subTitle: 'OPTIMIZING UX STAKEHOLDER ENGAGEMENT THROUGH COLLABORATIVE STRATEGIES.',
+  subTitle: 'DISCOVERING THE MID GROUND BETWEEN UI, GRAPHICS AND MOTION DESIGN',
   time: '7pm - 8pm (WAT)',
-  date: 'April 2nd, 2023',
+  date: 'April  16th, 2023',
   location: 'GOOGLE MEET',
   bannerImage: '/assets/images/banner/bannerImage.jpeg',
   description:
-    "The speaker will discuss practical strategies and best practices for effective collaboration with stakeholders that can help you improve your work as a UX designer. ",
-  content1: "By attending this session, you will gain valuable insights on how to engage stakeholders, get their buy-in and involvement in the design process, and build a strong collaborative relationship that can lead to better outcomes for your projects.",
+    "In today's digital landscape, the lines between user interface (UI), graphic design, and motion design are becoming increasingly blurred. While each discipline has its own unique set of skills and techniques, there is often overlap in their application, and collaboration between these areas is becoming more and more essential to creating effective and engaging digital experiences. ",
+  content1: "This session will explore the mid ground between UI, graphics, and motion design, and will feature insights and strategies from experts in each discipline. Participants will gain a deeper understanding of the interplay between these areas, and will learn practical tips and techniques for enhancing collaboration and achieving better results.",
   content2: "",
   content3: "",
   content4: "",
-  speakerName: 'Parklins Ifeanyichukwu',
-  speakerRole: 'Lead UX Designer, InMobi',
+  speakerName: 'Ufot Ubon',
+  speakerRole: 'Product Designer & Illustrator',
 }
