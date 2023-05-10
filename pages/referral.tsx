655c97cbd857e2fcdda300656d77d343a09757c8
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-05-10 13:40:14 +0100

    fixed

--- pages/referral.tsx
+++ pages/referral.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Hero, HowItWorks, RefClass, ReferQuestions} from '../features/referral'
+import { Footer } from '../components'
 const referral = () => {
   return (
     <div>
@@ -7,6 +8,7 @@ const referral = () => {
       <HowItWorks />
       <RefClass/>
       <ReferQuestions/>
+      <Footer/>
     </div>
   )
 }
