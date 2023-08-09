911a6dbb4f183bbb188a22a8e9c65b9b51abc7a3
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-08-09 11:54:53 +0100

    fixed

--- features/masterclass/EnrolForm.tsx
+++ features/masterclass/EnrolForm.tsx
@@ -29,7 +29,7 @@ const EnrolForm = () => {
         title={`Thank you for your registering for the ${classVal} Masterclass!`}
         description="Join the Class by clicking on the button."
         buttonTitle="Join Class"
-        buttonHref={'https://chat.whatsapp.com/CAye6G9gng832emONzNUkZ'}
+        buttonHref={'https://chat.whatsapp.com/DT2daJjhl8D381BuiMr6I9'}
       />
       <Box
         className="enrol-form"
