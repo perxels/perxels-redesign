9c58595916fae72337148114f0bd2138d63d6511
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-09-06 20:44:38 +0100

    fixed

--- features/hub/NewHero.tsx
+++ features/hub/NewHero.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Box, Text, Heading, Button, Flex, Image} from '@chakra-ui/react'
 
 export const NewHero = () => {
+  useEffect(() => {
+    const video = document.querySelector('video')
+    video?.addEventListener('loadeddata', () => {
+      video.play()
+    })
+  }, [])
   return (
     <Box pb="4.375rem"
     background={'url("/assets/images/hub/hubHeroBg.png") no-repeat center center'}
@@ -20,7 +26,7 @@ export const NewHero = () => {
     </Box>
     <Box mt={["3.125rem","144px"]}  w="100%" px={["0","10%"]}  h={["auto", "auto"]} overflow={"hidden"}>
         {/* <Image src="/assets/images/hub/hubHero.png" alt="hero"  h={["565px", "auto"]}  objectFit={["cover", "contain"]} /> */}
-        <video width="100%" height="565px" autoPlay muted loop
+        <video width="100%" height="565px" autoPlay muted loop playsInline
         src="https://res.cloudinary.com/deudl0ryy/video/upload/v1693844203/PERXELS_compressed_l9ogx0.mp4"
         />
         {/* <Box as="iframe" src="https://res.cloudinary.com/deudl0ryy/video/upload/v1693844203/PERXELS_compressed_l9ogx0.mp4"  allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen w="100%" h={["565px", "548px"]}  objectFit={["cover", "contain"]} /> */}
