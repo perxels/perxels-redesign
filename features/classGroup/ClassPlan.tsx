cad447d1f59ab28988ff88d4b286704133dd6d9d
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2023-11-02 10:28:22 +0100

    fixed

--- features/classGroup/ClassPlan.tsx
+++ features/classGroup/ClassPlan.tsx
@@ -7,7 +7,7 @@ export const OnlineClassPlan = () => {
     <Flex
     justifyContent="space-between"
     backgroundColor="#FDE85C"
-    padding={["4.5625rem 1.0625rem 3.625rem 1.25rem", "2.625rem 2.5rem 2.375rem 5rem"]}
+    padding={["3.5625rem 1.0625rem 3.625rem 1.25rem", "2.625rem 2.5rem 2.375rem 5rem"]}
     borderRadius="1.25rem"
     alignItems="center"
     >
@@ -21,25 +21,25 @@ export const OnlineClassPlan = () => {
         >
             <Heading
             color="#121212"
-            fontSize={["2.625rem","4.375rem"]}
+            fontSize={["2.125rem","4.375rem"]}
             fontWeight="800"
-            lineHeight={["2.9875rem","4.4625rem"]}
+            lineHeight={["2.5875rem","4.4625rem"]}
             >
                 Explore Our
             </Heading>
             <Heading
             color="#34296B"
-            fontSize={["2.625rem","4.375rem"]}
+            fontSize={["2.125rem","4.375rem"]}
             fontWeight="800"
-            lineHeight={["2.9875rem","4.4625rem"]}
+            lineHeight={["2.5875rem","4.4625rem"]}
             >
                 Online Classes.
             </Heading>
             <Text
             color="#555555"
-            fontSize="1.25rem"
+            fontSize={["0.9rem","1.25rem"]}
             fontWeight="500"
-            lineHeight="1.6812rem"
+            lineHeight={["1.1rem","1.6812rem"]}
             mt={[".75rem","1.875rem"]}
             >
             Uncover the world of design through our online classes. Learn, collaborate, and design with us, anytime, anywhere.
@@ -47,7 +47,7 @@ export const OnlineClassPlan = () => {
 
             <Button
             fontSize=".9375rem"
-            mt={[".6875rem","1.875rem"]}
+            mt={["1.6875rem","1.875rem"]}
             as={Link}
             href="/online"
             >
@@ -64,7 +64,7 @@ export const PhysicalClassPlan = () => {
       <Flex
       justifyContent="space-between"
       backgroundColor="#34296B"
-      padding={["4.5625rem 1.0625rem 3.625rem 1.25rem","2.625rem 2.5rem 2.375rem 3.75rem"]}
+      padding={["3.5625rem 0.6rem 3.625rem 0.6rem","2.625rem 2.5rem 2.375rem 3.75rem"]}
       borderRadius="1.25rem"
       flexDirection="row-reverse"
       alignItems="center"
@@ -80,25 +80,25 @@ export const PhysicalClassPlan = () => {
           >
               <Heading
               color="#FFF"
-              fontSize={["2.625rem","4.375rem"]}
-              fontWeight="800"
-              lineHeight={["2.9875rem","4.4625rem"]}
+              fontSize={["2rem","4.375rem"]}
+            fontWeight="800"
+            lineHeight={["2.5875rem","4.4625rem"]}
               >
                   Explore Our
               </Heading>
               <Heading
               color="#FDE85C"
-              fontSize={["2.625rem","4.375rem"]}
-              fontWeight="800"
-              lineHeight={["2.9875rem","4.4625rem"]}
+              fontSize={["2rem","4.375rem"]}
+            fontWeight="800"
+            lineHeight={["2.5875rem","4.4625rem"]}
               >
                  Physical Classes.
               </Heading>
               <Text
               color="#FFF"
-              fontSize="1.25rem"
+              fontSize={["0.9rem","1.25rem"]}
               fontWeight="500"
-              lineHeight="1.6812rem"
+              lineHeight={["1.1rem","1.6812rem"]}
               mt={[".75rem","1.875rem"]}
               >
              On our quest to continue expanding and bringing design education to your dorrstep, we have establishde a couple of physical learning spaces.
@@ -106,7 +106,7 @@ export const PhysicalClassPlan = () => {
   
               <Button
               fontSize=".9375rem"
-              mt={[".6875rem","1.875rem"]}
+              mt={["1.6875rem","1.875rem"]}
               color="#34296B"
               background="#FFF"
               as={Link}
