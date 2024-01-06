bf0af4bd003e8472e785c1c7aa4df0f1f96e2ad3
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2024-01-06 01:12:14 +0100

    Fixed

--- features/retreat/EventCard.tsx
+++ features/retreat/EventCard.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import {Box, Text, Heading, Image} from '@chakra-ui/react'
+import {Box, Text, Heading} from '@chakra-ui/react'
 import {RetreatInfoInterface} from '../../constant'
-
+import Image from 'next/image'
 
 export const EventCard = ({title, icon, content, fontSize, bgColor}:RetreatInfoInterface ) => {
   return (
     <Box backgroundColor={bgColor}  py="3.5625rem" display="flex" alignItems="center" flexDir="column">
-        <Image src={icon} alt="retreat4" />
+        <Image src={icon} alt="retreat4" width="60" height="60"/>
         <Heading lineHeight="110%" fontFamily="Montserrat" fontSize={fontSize} fontWeight="900" mt="1.25rem">
         {title}
         </Heading>
