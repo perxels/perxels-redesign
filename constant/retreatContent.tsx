e30d7e071cf9168e12adbbbde01f9127cbd76713
Segun Tuase <59211245+Tuasegun@users.noreply.github.com>
2024-01-06 01:33:33 +0100

    Fixed

--- constant/retreatContent.tsx
+++ constant/retreatContent.tsx
@@ -11,21 +11,21 @@ export const InfoContent : RetreatInfoInterface[] = [
     {
         title: "₦5000",
         content: "Access Fee",
-        iconSrc: "/assets/icons/icon1.svg",
+        iconSrc: "/icon1.svg",
         fontSize: "4.0625rem",
         bgColor: "#F0FFDF"
     },
     {
         title: "FEB 4, 11AM",
         content: "Event Date",
-        iconSrc: "/assets/icons/icon4.svg",
+        iconSrc: "/icon4.svg",
         fontSize: "2.5rem",
         bgColor: "#FFFCDF"
     },
     {
         title: "Lekki",
         content: "Venue",
-        iconSrc: "/assets/icons/icon2.svg",
+        iconSrc: "/icon2.svg",
         fontSize: "4rem",
         bgColor: "#FCEBFF"
     }
