1e020fa6fdf9c31f6f4b9c97997bd93df4129557
Sewo Goodness <sewogoodness111@gmail.com>
2024-05-23 19:26:40 +0100

    updated current banner

--- constant/bannerContent.tsx
+++ constant/bannerContent.tsx
@@ -16,18 +16,19 @@ interface BannerContent {
 
 export const bannerContent: BannerContent = {
   mainTitle: 'DESIGN DIGEST SESSION: ',
-  subTitle: 'Strategies for Enhancing UX After Product Launch',
+  subTitle: 'Double Diamond UX Process',
   time: '7pm-8pm',
-  date: '5th MAY, 2024',
+  date: '2nd June, 2024',
   location: 'GOOGLE MEET',
-  bannerImage: '/assets/images/banner/bannerImage3.jpg',
+  bannerImage: '/assets/images/banner/bannerImage2.jpg',
   description:
-    'Join us for an Exclusive Q&A session with OLABINJO EBENEZER.  A Senior User Research and Product Designer boasting 9 years of experience. He started in brand design and visual perception, but he shifted to Product Design about 5 years back, with a strong emphasis on User Research as he delves into the intricacies of strategies of enhancing UX after product launch offering invaluable insights that will undoubtedly enrich your design journey.',
-  content1:
-    "Don't miss this opportunity to grasp what Product Launch is all about, what it means to enhance UX and the Strategies, and how they are to be implemented after the product Launch. ",
-  content2: '',
-  content3: '',
-  content4: '',
-  speakerName: 'OLABINJO EBENEZER',
+    'Join us for an Exclusive Q&A session with OLUWASEUN AYENI.  A Seasoned UI/UX Designer with over 5 years of experience in creating intuitive B2B, B2C, and SaaS products across diverse industries; adept at leading global design teams and driving user engagement through innovative design solutions. With a proven track record in mentoring, enhancing client conversion by 20%, and spearheading projects that exceed user expectations and business objectives.as he delves into the intricacies of the Double Diamond UX Process  offering invaluable insights that will undoubtedly enrich your design journey.',
+  content1: "Don't miss this opportunity to grasp:",
+  content2: 'the principles and stages of the Double Diamond UX process',
+  content3:
+    'the objectives and key activities associated with each stage of the Double Diamond framework.',
+  content4:
+    'how to effectively apply divergent and convergent thinking techniques within the Double Diamond process.',
+  speakerName: 'Design Team Lead at SBSC',
   speakerRole: 'Senior User Research and Product Designer',
 }
