9c6583701772d854f3d4f63908845f6fabb92ebc
Sewo Goodness <sewogoodness111@gmail.com>
2024-07-10 15:58:42 +0100

    ADDED Banner

--- constant/bannerContent.tsx
+++ constant/bannerContent.tsx
@@ -16,20 +16,21 @@ interface BannerContent {
 
 export const bannerContent: BannerContent = {
   mainTitle: 'DESIGN DIGEST SESSION: ',
-  subTitle: 'Understanding Grouping and Framing',
+  subTitle:
+    'User-Centric Solutions in Digital Transformation: Strategies and Challenge',
   time: '7pm-8pm',
-  date: '16th June, 2024',
+  date: '21st July, 2024',
   location: 'GOOGLE MEET',
-  bannerImage: '/assets/images/banner/bannerImage2.jpg',
+  bannerImage: '/assets/images/banner/bannerImage.jpeg',
   description:
-    'Join us for an Exclusive Q&A session with Samuel Olayanju',
-  content1:
-    "This session will focus on helping the audience understand:",
+    'Join us for an Exclusive Q&A session with Samuel Olumoyeke. Samuel is a Senior UX/UI Designer at CtrlChain, a digital freight forward firm specializing in simplifying and automating logistics management. With a BSc. in Mathematics Education from the University of Lagos (Unilag), he has successfully combined his analytical skills and creative talent to design intuitive, user-friendly digital solutions. His role at CtrlChain involves enhancing the company’s service delivery through innovative and engaging user experiences offering invaluable insights that will undoubtedly enrich your design journey.',
+  content1: 'Don’t miss this opportunity to grasp:',
   content2:
-    'the concepts of grouping and framing in the context of decision-making and cognitive psychology;',
+    'the importance of user-centric solutions in the context of digital transformation.',
   content3:
-    'different types of grouping techniques and framing effects commonly encountered in various domains.',
-  content4: 'the psychological mechanisms underlying grouping and framing and their influence on perception and judgment.',
-  speakerName: 'SAMUEL OLAYUNJU',
-  speakerRole: 'Product Designer',
+    'Identify key strategies for implementing user-centric approaches during digital transformation initiatives.',
+  content4:
+    'the challenges and barriers associated with integrating user-centric solutions into digital transformation efforts.',
+  speakerName: 'Samuel Olumoyeke',
+  speakerRole: 'Senior UX/UI Designer',
 }
