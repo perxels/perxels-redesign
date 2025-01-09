a19b89e52e0ee44f83541a4b10e119ad0614729c
Sewo Goodness <sewogoodness111@gmail.com>
2025-01-09 21:50:48 +0100

    feat: added new yabaclass page

---
+++ constant/yabaFeatsContent.ts
@@ -0,0 +1,39 @@
+export interface YabaFeatProps {
+    icon: string;
+    title: string;
+    description: string;
+}
+
+export const YabaFeatsContent: YabaFeatProps[] = [
+    {
+        icon: '/assets/images/hub/hubFeat1.svg',
+        title: 'Master the tools',
+        description: 'Master cutting-edge tools like Figma, Figjam, Adobe Illustrator, and Miro.'
+    },
+    {
+        icon: '/assets/images/hub/hubFeat2.svg',
+        title: 'Stunning Designs',
+        description: 'Create stunning designs for UI/UX, branding, and marketing.'
+    },
+    {
+        icon: '/assets/images/hub/hubFeat3.svg',
+        title: 'Standard Portfolio',
+        description: 'Build a professional portfolio that opens doors to top clients and employers.',
+    },
+    {
+        icon: '/assets/images/hub/hubFeat4.svg',
+        title: 'Job Oppoturnity',
+        description: 'Gain job-ready skills, including collaboration with product managers and developers.'
+    },
+    {
+        icon: '/assets/images/hub/hubFeat5.svg',
+        title: 'Guaranteed Internship',
+        description: "Access internships and career opportunities through our extensive network."
+    },
+    {
+        icon: '/assets/images/hub/hubFeat6.svg',
+        title: 'Distraction free space',
+        description: 'Experience a quiet environment for focused work.'
+    }
+
+]
