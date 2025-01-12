5facd059635681c8fe9e13e3a9bbe7734f63aa6e
Sewo Goodness <sewogoodness111@gmail.com>
2025-01-12 15:17:41 +0100

    fix: setup facebook tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -58,30 +58,39 @@ export default function App({ Component, pageProps }: AppProps) {
         `,
         }}
       />
-      <Script
-        strategy="afterInteractive"
+
+     
+
+       {/* Facebook Pixel Code */}
+    <Script
         id="facebook-pixel"
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window,document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-         fbq('init', '821337219169131'); 
-        fbq('track', 'PageView');
-        `,
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '309688088029898'); 
+            fbq('track', 'PageView');
+          `,
         }}
       />
+      {/* NoScript Fallback */}
       <noscript
         dangerouslySetInnerHTML={{
           __html: `
-        <img height="1" width="1" 
-        src="https://www.facebook.com/tr?id=821337219169131&ev=PageView
-        &noscript=1"/>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          alt=""
+          src="https://www.facebook.com/tr?id=309688088029898&ev=PageView&noscript=1"
+        />
         `,
         }}
       />
