9b97631e0360b8bc535d09deb5050b3bba9c3553
EmmanuelOluwafemi <emaz4me@gmail.com>
2025-05-01 17:56:36 +0100

    update library page

--- features/library/VideoCardLayout.tsx
+++ features/library/VideoCardLayout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useMemo } from 'react'
 import {
   SimpleGrid,
   Box,
@@ -14,8 +14,26 @@ import {
 import { VideoCard } from './VideoCard'
 import { LibraryAd } from './LibraryAd'
 import { useFetchVideos } from '../../hooks/useVideos'
+import { Video } from '../../utils/types'
+
 export const VideoCardLayout = () => {
   const { videos, loading } = useFetchVideos()
+
+  const sortedByOrder = useMemo(() => {
+    // Create a copy of the videos array to avoid mutating the original
+    return [...videos].sort((a: Video, b: Video) => {
+      // If both videos have order, sort by order
+      if (a.order !== undefined && b.order !== undefined) {
+        return a.order - b.order
+      }
+      // If only one has order, put the one with order first
+      if (a.order !== undefined) return -1
+      if (b.order !== undefined) return 1
+      // If neither has order, maintain original order
+      return 0
+    })
+  }, [videos])
+
   return (
     <Box>
       <SimpleGrid py="10%" columns={[1, 2, 2]} spacing="32px">
@@ -30,9 +48,9 @@ export const VideoCardLayout = () => {
           </Stack>
         ) : (
           <>
-            {videos.map((item, i) => {
-              return <VideoCard {...item} key={i} />
-            })}
+            {sortedByOrder.map((item) => (
+              <VideoCard {...item} key={item.id} />
+            ))}
           </>
         )}
       </SimpleGrid>
