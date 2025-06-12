21402749ff5a923220f48269a75e6254c0e132d7
EmmanuelOluwafemi <emaz4me@gmail.com>
2025-06-12 18:57:42 +0100

    feat: add facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -37,6 +37,31 @@ export default function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
 
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+      !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '821337219169131');
+fbq('track', 'PageView');
+    `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=821337219169131&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <title key="title">Perxels - Design School</title>
       </Head>
       <Script
@@ -59,10 +84,8 @@ export default function App({ Component, pageProps }: AppProps) {
         }}
       />
 
-     
-
-       {/* Facebook Pixel Code */}
-    <Script
+      {/* Facebook Pixel Code */}
+      <Script
         id="facebook-pixel"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
