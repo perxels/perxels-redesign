0823f93c05bb9026e55f63d1616b5ff02864bdda
EmmanuelOluwafemi <emaz4me@gmail.com>
2025-08-22 01:58:00 +0100

    feat: changed to new url

--- pages/portal.tsx
+++ pages/portal.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { WelcomePortal } from '../features/portal/welcome'
-// import {PortalHero} from '../features/portal'
+// import { WelcomePortal } from '../features/portal/welcome'
+import {PortalHero} from '../features/portal'
 const portal = () => {
   return (
     <div>
-      {/* <PortalHero /> */}
-      <WelcomePortal type="student" />
+      <PortalHero />
+      {/* <WelcomePortal type="student" /> */}
     </div>
   )
 }
